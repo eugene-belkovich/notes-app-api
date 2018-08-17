9e7fc0395973dcd938e78ba36c94b4c30fbb1393
Eugene Belkovich <eugene.belkovich@gmail.com>
2018-08-17 17:40:16 +0300

    edit update function, added delete function

--- delete.js
+++ delete.js
@@ -2,23 +2,16 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
-  const data = JSON.parse(event.body);
   const params = {
     TableName: "notes",
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: event.pathParameters.id
-    },
-    UpdateExpression: "SET content = :content, attachment = :attachment",
-    ExpressionAttributeValues: {
-      ":attachment": data.attachment ? data.attachment : null,
-      ":content": data.content ? data.content : null
-    },
-    ReturnValues: "ALL_NEW"
+    }
   };
 
   try {
-    await dynamoDbLib.call("update", params);
+    await dynamoDbLib.call("delete", params);
     callback(null, success({ status: true }));
   } catch (e) {
     callback(null, failure({ status: false }));
